d2758f8bdbd28b70f84e7960c5f97a982fec8d9d
Frederico Silva <fredericochopin@gmail.com>
2016-06-22 22:40:23 -0300

    Finish level 1 - roadtrip I

---
+++ javascript-roadtrip-I/exercises.js
@@ -0,0 +1,40 @@
+/*
+BOOLEANS - comparisons, operators
+*/
+//1.2
+2*18;
+
+//1.3
+(3 + 4) * 2;
+
+//1.4 - Modulus
+44%10;
+
+//1.5
+8==7;
+
+//1.6
+9!=125;
+
+
+/*
+STRINGS - simple text
+*/
+//1.8
+"Francis Smith";
+
+/*1.9 - Special characters
+    \n - place in a new line
+    \t - tabspace
+    \" - avoid evaluating reserved characters (like ")
+*/
+"Quoth the raven:\n\t\"Nevermore!\"";
+
+//1.10 - Compare two strings
+"Formidable weather" == "Formidable weather";
+
+//1.11 - Concatenation
+"eight " + "5 " + "six " + "two " + "3 " + "7";
+
+//1.12 - Length of a string
+"All work no play makes Jack a boy".length;
