1882f37613af77d744e353e091134c00e55ecde6
Frederico Silva <fredericochopin@gmail.com>
2016-06-23 23:25:28 -0300

    Variables

---
+++ javascript-roadtrip-I/2-variable-valley.js
@@ -0,0 +1,39 @@
+//2.2 - Variable declaration and assignment
+var age = 28;
+
+//2.3
+var age = 10
+var ageIn5Years = age + 5;
+
+//2.5
+var firstName = "Fred";
+
+//2.6
+var lastName = "Smith";
+
+//2.7
+var fullName = firstName + " " + lastName;
+
+//2.8 - charAt(position) - function that retrieves the character at the specified position of a String
+var index14 = "What can we do? Keep going!".charAt(14);
+
+//2.9 - length - property that indicates the number of charachters of a String
+var sentence = "This variable is already assigned. Find its length without manually counting the characters and spaces.";
+sentence.length;
+
+//2.10
+var numPassengers = 0;
+numPassengers+=3;
+
+//2.11
+var countMessage = "Attention: There are now " + numPassengers +" passengers on the train!";
+
+//2.12
+var occupancy = numPassengers + " passengers present, with " + (trainCapacity - numPassengers) + " seats remaining.";
+
+//2.13
+var twenty20 = 3 + 10 + 7;
+
+//2.14
+twenty20 = "Never look back.".charAt(0) + "Adventure is out there.".charAt(3) + "Just keep swimming.".charAt(14) + "To infinity... and beyond!".charAt(1);
+
