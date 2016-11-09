8cd7a382234e070a6bce447d991508da4f3ead58
Frederico Silva <fredericochopin@gmail.com>
2016-11-09 09:56:00 -0200

    Prototypes III

--- javascript-roadtrip-III/5-plains-of-prototypes.js
+++ javascript-roadtrip-III/5-plains-of-prototypes.js
@@ -1,6 +1,6 @@
 /*
-PROTOTYPE: every object in Javascript has inherit from the Object Protoype.
-From it, all other properties are passed down, like valurOf, hasOwnProperty, etc.
+PROTOTYPE: every object in Javascript has inheritance from the Object Protoype.
+From it, all other properties are passed down, like valueOf, hasOwnProperty, etc.
 
 ARRAY PROTOTYPE
     An ARRAY object, for exemple, inherits its properties (methods) like pop(), push(), shift(), reverse(), sort(), etc from the Array Prototype.
@@ -165,3 +165,184 @@ var numPriorityCows = canyonCows.countForBreeding() +
     badlandsCows.countForBreeding();
 
 alert("Herd-merger has indicated " + numPriorityCows + " cows of top breeding priority.");
+
+/*
+OBJECT.CREATE = using inheritance, we can create objects using our objects as prototypes
+*/
+var shoe = {size: 6, gender: "women", construction: "zipper"};
+
+var magicShoe = Object.create(shoe);
+
+magicShoe.jewels = "ruby";
+magicShoe.travelAction = "click heels";
+magicShoe.actionsRequired = 3;
+
+console.log(magicShoe);
+//The prototype will remain unchanged
+console.log(shoe);
+
+Object.prototype.isPrototypeOf(shoe);
+//true
+
+shoe.isPrototypeOf(magicShoe);
+//true
+
+magicShoe.isPrototypeOf(magicShoe);
+//false
+
+Object.prototype.isPrototypeOf(magicShoe);
+//true (more than one level of inheritance)
+
+/* That way of inheriting will pass in the values of the prototype. 
+For example, if we want a men's boot and use shoe as the prototype, all property values are set to women.
+
+CLASS: a set of objects that all share and inherit from the same basic prototype.
+A CONSTRUCTOR allows us to set up inheritance while also assigning specific property values.
+*/
+
+//The capitalized function will distinguish what is a constructor
+function Shoe(shoeSize, shoeColor, forGender, constructStyle){
+    //the THIS keyword will automatically refer to the new instance of the class that is being made
+    this.shoeSize = shoeSize;
+    this.shoeColor = shoeColor;
+    this.forGender = forGender;
+    this.constructStyle = constructStyle;
+}
+
+//The NEW keyword asks to build a new instance of something, in this case, a Shoe
+var beachShoe = new Shoe(10, "blue", "women", "flipflop");
+beachShoe.straps = 2;
+console.log(beachShoe);
+
+/*
+By setting a constructor prototype, every new instance will refer to it for extra properties.
+It does not have to be created in every instance of the class, since it does not change according to the property values.
+*/
+Shoe.prototype = {
+    putOn: function(){alert("Shoe's on!");},
+    takeOff: function(){alert("Uh, what's that smell?");};
+};
+
+//looks into itself
+beachShoe.forGender();
+
+//looks into the prototype
+beachShoe.takeOff();
+
+//hasOwnProperty is from Object Prototype - this line will inherit this property (method) from Object Prototype and check
+//if construction is a property from beachShoe (Shoe class) and not from another prototype;
+beachShoe.hasOwnProperty("construction");
+
+/*
+Prototypes can also refer back to the instance
+We can modify the message functions in our prototype to use the data values in the calling instances
+*/
+Shoe.prototype = {
+    putOn: function(){alert("Your " + this.construction + "'s on!");},
+    takeOff: function(){alert("Phew! Somebody's size " + this.size + "'s are fragrant!");};
+};
+
+//5.7
+var genericPost = {
+  x: 0,
+  y: 0,
+  postNum: undefined,
+  connectionsTo: undefined,
+  sendRopeTo: function(connectedPost) {
+    if (this.connectionsTo == undefined) {
+      var postArray = [];
+      postArray.push(connectedPost);
+      this.connectionsTo = postArray;
+    } else {
+      this.connectionsTo.push(connectedPost);
+    }
+  }
+};
+
+var post1 = Object.create(genericPost);
+var post2 = Object.create(genericPost);
+
+post1.x = -2;
+post1.y = 4;
+post1.postNum = 1;
+
+post2.x = 5;
+post2.y = 1;
+post2.postNum = 2;
+
+post1.sendRopeTo(post2);
+post2.sendRopeTo(post1);
+
+//5.8
+var post8 = Object.create(genericPost);
+var post9 = Object.create(genericPost);
+var post10 = Object.create(genericPost);
+
+post8.x = 0;
+post8.y = -3;
+post8.postNum = 8;
+post8.sendRopeTo(post10);
+
+post9.x = 6;
+post9.y = 8;
+post9.postNum = 9;
+post9.sendRopeTo(post10);
+
+post10.x = -2;
+post10.y = 3;
+post10.postNum = 10;
+post10.sendRopeTo(post8);
+post10.sendRopeTo(post9);
+
+post9.numBirds = 0;
+post10.weathervane = "N";
+post8.lightsOn = false;
+post10.lightsOn = false;
+
+//5.9
+function Fencepost(x, y, postNum){
+    this.x = x;
+    this.y = y;
+    this.postNum = postNum;
+    this.connectionsTo = [];
+    this.sendRopeTo = function(connectedPost) {
+        this.connectionsTo.push(connectedPost);
+    };
+}
+
+//5.10
+var post18 = new Fencepost(-3, 4, 18);
+var post19 = new Fencepost(5,-1, 19);
+var post20 = new Fencepost(-2, 10, 20);
+
+post18.sendRopeTo(post20);
+post20.sendRopeTo(post18);
+post18.sendRopeTo(post19);
+post19.sendRopeTo(post18);
+
+//5.11
+function Fencepost(x, y, postNum) {
+  this.x = x;
+  this.y = y;
+  this.postNum = postNum;
+  this.connectionsTo = [];
+}
+
+Fencepost.prototype = {
+    sendRopeTo: function(connectedPost) {
+        this.connectionsTo.push(connectedPost);
+    },
+    removeRope: function(removeTo) {
+        var temp = [];
+        for (var i = 0; i < this.connectionsTo.length; i++) {
+            if (this.connectionsTo[i].postNum != removeTo) {
+                temp.push(this.connectionsTo[i]);
+            }
+        }
+        this.connectionsTo = temp;
+    },
+    movePost: function(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+};
